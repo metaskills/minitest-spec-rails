0618fb3d23c01366e5aec1fe8b23a99b8db8f508
Jack Chu <kamuigt@gmail.com>
2012-05-18 14:28:03 -0400

    use describe block instead of class definition controller name detection define @controller and @routes for controller tests define @app for integration tests

--- lib/minitest-spec-rails/test_case.rb
+++ lib/minitest-spec-rails/test_case.rb
@@ -1,3 +1,42 @@
-class << ActiveSupport::TestCase
-  remove_method :describe
-end if defined?(ActiveSupport::TestCase)
+if defined?(ActiveSupport::TestCase)
+  class << ActiveSupport::TestCase
+    remove_method :describe
+  end
+
+  MiniTest::Spec.register_spec_type ActiveSupport::TestCase do |desc|
+    desc.respond_to?(:ancestors) && !(desc.ancestors.map(&:to_s) & ["ActiveRecord::Base", "Mongoid::Document", "MongoMapper::Document", "DataMapper::Resource"]).empty?
+  end
+end
+
+if defined?(ActionController::TestCase)
+  class ActionController::TestCase
+    def self.determine_default_controller_class(name)
+      if name.match(/.*(?:^|::)(\w+Controller)/)
+        $1.safe_constantize
+      else
+        super(name)
+      end
+    end
+
+    before do
+      @controller = self.class.name.match(/((.*)Controller)/)[1].constantize.new
+      @routes     = ::Rails.application.routes
+    end
+
+    subject do
+      @controller
+    end
+  end
+
+  MiniTest::Spec.register_spec_type /Controller/, ActionController::TestCase
+end
+
+if defined?(ActionDispatch::IntegrationTest)
+  class ActionDispatch::IntegrationTest
+    before do
+      @app = ::Rails.application
+    end
+  end
+
+  MiniTest::Spec.register_spec_type /Integration Test$/, ActionDispatch::IntegrationTest
+end
