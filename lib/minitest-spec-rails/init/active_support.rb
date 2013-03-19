6693c8685d7e6581c6ebd3c11e986f5d660ae882
Anton Lindqvist <anton@qvister.se>
2013-03-19 20:21:02 +0100

    Register non ActiveRecord::Base classes correctly.

--- lib/minitest-spec-rails/init/active_support.rb
+++ lib/minitest-spec-rails/init/active_support.rb
@@ -8,7 +8,7 @@ module MiniTestSpecRails
         singleton_class.class_eval { remove_method :describe }
         extend MiniTest::Spec::DSL
         include MiniTestSpecRails::DSL
-        register_spec_type(self) { |desc| Class === desc && desc < ActiveRecord::Base if defined?(ActiveRecord::Base) }
+        register_spec_type(self) { |desc| Class === desc }
         register_rails_test_case self
       end
 
---
+++ test/dummy_tests/library_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper_dummy'
+require_relative '../dummy_app/lib/library'
+
+class LibraryTest < ActiveSupport::TestCase
+  it 'reflects' do
+    describing_class.must_equal LibraryTest
+    described_class.must_equal Library
+  end
+end
+
+describe Library do
+  it 'reflects' do
+    described_class.must_equal Library
+    self.class.described_class.must_equal Library
+  end
+end
