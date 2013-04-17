ed35b2d142e57d3f875cbe3421cb440626e7a852
Ken Collins <ken@metaskills.net>
2013-04-17 08:24:59 -0400

    Test assert_routing.

--- test/dummy_tests/integration_test.rb
+++ test/dummy_tests/integration_test.rb
@@ -11,6 +11,10 @@ module IntegrationTests
       status.must_equal 200
     end
 
+    it 'works with assert_routing' do
+      assert_routing '/', :controller => 'application', :action => 'index'
+    end
+
     it 'can find the app' do
       app.must_be_instance_of Dummy::Application
     end
@@ -36,4 +40,10 @@ end
 
 class IntegrationTest < ActionDispatch::IntegrationTest
   include IntegrationTests
+end
+
+class AppTest < ActionDispatch::IntegrationTest
+  def test_homepage
+    assert_routing '/', :controller => 'application', :action => 'index'
+  end
 end
